1e290d5e2b0c875ab26c6be2edc31e025a826017
Nuno Miguel <nm.albano20@gmail.com>
2022-05-26 22:37:18 +0100

    frent end

--- inner-page.php
+++ inner-page.php
@@ -35,14 +35,20 @@
     <section class="inner-page">
       <div class="container">
         <div class="row">
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <?php
+          while ($row = $result->fetch_object()) {
+            echo "<div class='card' style='width: 18rem'>";
+            echo <img class="card-img-top" src="..." alt="Card image cap">
+           echo <div class="card-body">
+              echo <h5 class="card-title">Card title</h5>
+              echo <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
-          </div>
+         echo"</div>";
+          }
+          ?>
+        
+          
         </div>
       </div>
     </section>
