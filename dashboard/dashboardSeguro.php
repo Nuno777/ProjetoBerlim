2e622cf8a9b1e3d0dbcf79caef0ff34f112f8edd
Nuno Miguel <nm.albano20@gmail.com>
2022-06-24 03:26:54 +0100

    reformer table seguro

--- dashboard/dashboardSeguro.php
+++ dashboard/dashboardSeguro.php
@@ -8,6 +8,7 @@ if (!isset($_SESSION['authenticated'])) {
 require_once '../conecao.php';
 $query = "SELECT * FROM cliente ORDER BY IDcliente";
 $result = mysqli_query($conn, $query);
+$resultdelete = mysqli_query($conn, $query);
 ?>
 <!doctype html>
 <html class="no-js" lang="en">
@@ -37,12 +38,10 @@ $result = mysqli_query($conn, $query);
                                         <table class="table table-hover text-center">
                                             <thead class="text-uppercase bg-dark">
                                                 <tr class="text-white">
-                                                    <th scope="col">ID</th>
-                                                    <th scope="col">Primeiro Nome</th>
-                                                    <th scope="col">Ultimo Nome</th>
                                                     <th scope="col">Email</th>
-                                                    <th scope="col">Morada</th>
+                                                    <th scope="col">Nome</th>
                                                     <th scope="col">Localidade</th>
+                                                    <th scope="col">Morada</th>
                                                     <th scope="col">Código Postal</th>
                                                     <th scope="col">NIF</th>
                                                     <th scope="col">Pacote</th>
@@ -54,11 +53,11 @@ $result = mysqli_query($conn, $query);
                                                 <?php
                                                 while ($row = $result->fetch_object()) {
                                                     echo "<tr>";
-                                                    echo "<td>" . $row->IDcliente . "</td><td>" . $row->nome_primeiro . "</td><td>" . $row->nome_ultimo . "</td>";
-                                                    echo "<td>" . $row->email . "</td><td>" . $row->rua . "</td><td>" . $row->localidade . "</td>";
-                                                    echo "<td>" . $row->cpostal . "</td><td>" . $row->nif . "</td><td>" . $row->pacote . "</td>";
+                                                    echo "<td>" . $row->email . "</td><td>" . $row->nome_primeiro . " " . $row->nome_ultimo . "</td>";
+                                                    echo "<td>" . $row->localidade . "</td><td>" . $row->rua . "</td><td>" . $row->cpostal . "</td>";
+                                                    echo "<td>" . $row->nif . "</td><td>" . $row->pacote . "</td>";
                                                     echo "<td><a href='editseguro.php?IDcliente=$row->IDcliente' class='text-secondary' name='edit'><i class='ti-pencil-alt'></i></a></td>";
-                                                    echo "<td><a href='deleteseguro.php?IDcliente=$row->IDcliente' class='text-danger' name='delete'><i class='ti-trash'></i></a></td>";
+                                                    echo "<td><a data-toggle='modal' data-target='#deleteseguro$row->IDcliente' class='text-danger' name='delete'><i class='ti-trash'></i></a></td>";
                                                     echo "</tr>";
                                                 }
                                                 ?>
@@ -73,6 +72,29 @@ $result = mysqli_query($conn, $query);
             </div>
         </div>
     </div>
+
+    <!-- Modal para eliminar -->
+    <?php while ($row = $resultdelete->fetch_object()) { ?>
+        <div class="modal fade" id='deleteseguro<?php echo $row->IDcliente ?>' tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Eliminar Seguro</h5><span class="span-contat"><?php echo $row->email; ?></span>
+                    </div>
+                    <div class="modal-body">
+                        <p>Deseja eliminar este seguro de viagem?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
+                        <a href='deleteseguro.php?IDcliente=<?php echo $row->IDcliente ?>' type='button' class='btn btn-primary'>Sim</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
+    <!-- Modal para eliminar fechou -->
     <script src="assetsAdmin/js/vendor/jquery-2.2.4.min.js"></script>
     <script src="assetsAdmin/js/popper.min.js"></script>
     <script src="assetsAdmin/js/bootstrap.min.js"></script>
